d2256a93aa1a25df26644a88fb9346e23a635095
farkhan <farhanhamzah71@webmail.umm.ac.id>
2022-09-04 10:28:40 +0700

    create CRUD fro bootcamps

---
+++ routes/bootcamps.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { getBootcamps, createBootcamp, getBootcamp, updateBootcamp, deleteBootcamp } = require('../controller/bootcamps');
+const router = express.Router()
+
+router.route('/')
+    .get(getBootcamps)
+    .post(createBootcamp);
+
+router.route('/:id')
+    .get(getBootcamp)
+    .put(updateBootcamp)
+    .delete(deleteBootcamp);
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const cluster = require('cluster')
 const os = require('os')
+const morgan = require('morgan')
 const dotenv = require('dotenv')
+
+// Route files
+const bootcampsRoutes = require('./routes/bootcamps')
 
 // Load env vars
 dotenv.config({ path: './config/config.env' })
@@ -9,9 +13,12 @@ dotenv.config({ path: './config/config.env' })
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-    res.send('Hello fwom express')
-});
+// Middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('combined'))
+}
+
+app.use('/api/v1/bootcamps', bootcampsRoutes)
 
 if(cluster.isMaster) {
     console.log('Master has been started')

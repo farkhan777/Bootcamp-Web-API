5ee3c13d18dd465d2a0a5d39c1b955b8c1484c48
farkhan <farhanhamzah71@webmail.umm.ac.id>
2022-09-15 03:09:10 +0700

    Forgot password

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { register, login, getMe } = require('../controller/auth')
+const { register, login, getMe, forgotPassword, resetPassword } = require('../controller/auth')
 const { protect } = require('../middleware/auth')
 
 const router = express.Router()
@@ -7,5 +7,7 @@ const router = express.Router()
 router.post('/register', register)
 router.post('/login', login)
 router.get('/me',protect, getMe)
+router.post('/forgotPassword', forgotPassword)
+router.put('/resetpassword/:resettoken', resetPassword)
 
 module.exports = router

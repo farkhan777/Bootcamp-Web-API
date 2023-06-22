6cac1fa20327ad46630c67d66edaa09d3c0013aa
Farkhan Hamzah Firdaus <farhanhamzah71@webmail.umm.ac.id>
2023-06-22 12:01:50 +0700

    Fix mongo URI

--- config/db.js
+++ config/db.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose')
 // require('dotenv/config')
 require('dotenv').config()
 
+const MONGO_URI = process.env.MONGO_URI
+
 const connectDB = async () => {
-    console.log(process.env.MONGO_URI)
-    const conn = await mongoose.connect(process.env.MONGO_URI)
+    console.log(MONGO_URI)
+    const conn = await mongoose.connect(MONGO_URI)
 
     console.log(`MongoDB Connected ${conn.connection.host}`)
 }

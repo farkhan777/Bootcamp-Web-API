7a93add9f6f334b3555460f77e7915ca3c5bb322
Farkhan Hamzah Firdaus <farhanhamzah71@webmail.umm.ac.id>
2023-06-22 10:11:10 +0700

    change config

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const connectDB = async () => {
-    const conn = await mongoose.connect(process.env.MONGO_URI)
+    const conn = await mongoose.connect("mongodb://bootcamp:ZATn6nEsXHPZSLLn@ac-mul5wwo-shard-00-00.zprk9kd.mongodb.net:27017,ac-mul5wwo-shard-00-01.zprk9kd.mongodb.net:27017,ac-mul5wwo-shard-00-02.zprk9kd.mongodb.net:27017/bootcamp?ssl=true&replicaSet=atlas-go0nn2-shard-0&authSource=admin&retryWrites=true&w=majority")
 
     console.log(`MongoDB Connected ${conn.connection.host}`)
 }

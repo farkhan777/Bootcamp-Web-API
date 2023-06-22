b9479ddce3b967156ca7c0248913c8b402879a05
Farkhan Hamzah Firdaus <farhanhamzah71@webmail.umm.ac.id>
2023-06-22 11:35:15 +0700

    Change db connection

--- config/db.js
+++ config/db.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 // require('dotenv/config')
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
-
+require('dotenv').config()
 
 const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI)
